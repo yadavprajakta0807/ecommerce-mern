1f223336d4a9829720c005124a8434a94a20142e
Prajakta Yadav <yadavpraju2002@gmail.com>
2026-04-15 17:45:01 +0530

    use env variable for api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "https://ecommerce-mern-susr.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
-export default api ;
+export default api;
