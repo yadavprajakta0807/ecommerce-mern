5fb6b4f2f1d8f46372d086067b4a5c73b7d8d129
Prajakta Yadav <yadavpraju2002@gmail.com>
2026-04-15 16:14:10 +0530

    update api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : 'http://localhost:5001/api',
+    baseURL : "https://ecommerce-mern-susr.onrender.com",
 });
 
 export default api ;
